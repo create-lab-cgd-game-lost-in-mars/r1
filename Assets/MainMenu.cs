4e607bfbb9775739f4ff3c5598111e5636bc585e
LepronlineR <44305771+LepronlineR@users.noreply.github.com>
2019-07-09 18:07:40 -0400

    title screen 1

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioSource playAudio;
+    private IEnumerator DelayLoadgame()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void PlayGame()
+    {
+       // playAudio.Play();
+        StartCoroutine(DelayLoadgame());
+        Debug.Log("play");
+    }
+    public void QuitGame ()
+    {
+        Application.Quit();
+        Debug.Log("quit");
+    }
+}
