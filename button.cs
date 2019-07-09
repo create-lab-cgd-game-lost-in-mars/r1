f57edf75f40d5dfd54a36adc2c6617b063d429b6
1219-Eric <52709717+1219-Eric@users.noreply.github.com>
2019-07-09 19:26:24 -0400

    Add files via upload

---
+++ button.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class button : MonoBehaviour
+{
+    public string LevelName;
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Player>() == null)
+            return;
+        LevelManager.Instance.GotoNextLevel(LevelName);
+    }
+}
